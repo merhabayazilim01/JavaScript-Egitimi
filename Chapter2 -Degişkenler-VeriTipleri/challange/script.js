3146935b9e31fe9ab29e579e51ed164f4ae52222
muyeas <50539771+muyeas@users.noreply.github.com>
2024-01-27 13:03:03 +0300

    Chapter 2 dökümantasyon

---
+++ Chapter2 -Degişkenler-VeriTipleri/challange/script.js
@@ -0,0 +1,16 @@
+let sicaklik = 15
+let lokasyon = Bursa
+let kar_Varmi = false;
+
+//sicaklik convert string
+
+
+//kar_varmi convert number
+
+
+
+//kar_varmi convert eğer 1 ise kar var yok ise kar yok
+
+
+
+//dom ile hepsini yerleştir
